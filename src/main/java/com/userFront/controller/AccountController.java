ba6f602b9a2366985475ed164a51f71a28b135e3
ryhan000 <rayhan1ahamed@gmail.com>
2018-01-01 16:14:43 +0600

    added primary account page

--- src/main/java/com/userFront/controller/AccountController.java
+++ src/main/java/com/userFront/controller/AccountController.java
@@ -1,21 +1,38 @@
 package com.userFront.controller;
 
+import java.security.Principal;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.userFront.domain.PrimaryAccount;
+import com.userFront.domain.User;
+import com.userFront.service.UserService;
 
 @Controller
 @RequestMapping("/account")
 public class AccountController {
+	
+	
+	@Autowired
+    private UserService userService;
 
 	@RequestMapping("/primaryAccount")
-	public String primaryAccount() {
+	public String primaryAccount(Model model, Principal principal ) {
+	
+		User user = userService.findByUsername(principal.getName());
+        PrimaryAccount primaryAccount = user.getPrimaryAccount();
+
+        model.addAttribute("primaryAccount", primaryAccount);
 		
 		return "primaryAccount";
 	}
 	
 	
 	@RequestMapping("/savingsAccount")
-    public String savingsAccount() {
+    public String savingsAccount(Model model, Principal principal) {
 		
 		return "savingsAccount";
 	}
